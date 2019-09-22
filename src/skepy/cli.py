a228b1340f8293dd8bc2a7f69603ab453e85f95b
Yasuhiro0312 <boccochan@gmail.com>
2019-09-22 21:24:56 +0900

    Non optional mode

--- src/skepy/cli.py
+++ src/skepy/cli.py
@@ -2,11 +2,17 @@ import os
 import sys
 import json
 import click
+from typing import Optional
 from skepy.content import SETUP_PY, INIT_FILE, CLI, GITIGNORE
 
 
-def create_package_template(dir_path: str, package: str) -> None:
-    pkg_src = os.path.join(package, 'src', package)
+def create_package_template(dir_path: str, package: Optional[str]=None) -> None:
+    if package:
+        os.makedirs(package, exist_ok=True)
+        os.chdir(package)
+
+    pkg_src = os.path.join('src', os.getcwd().split(os.sep)[-1])
+
     os.makedirs(pkg_src, exist_ok=True)
 
     with open(os.path.join(pkg_src, '__init__.py'), 'w') as f:
@@ -15,17 +21,17 @@ def create_package_template(dir_path: str, package: str) -> None:
     with open(os.path.join(pkg_src, 'cli.py'), 'w') as f:
         f.write(CLI())
 
-    os.makedirs(os.path.join(package, 'tests'), exist_ok=True)
+    os.makedirs('tests', exist_ok=True)
 
-    with open(os.path.join(package, 'setup.py'), 'w') as f:
+    with open('setup.py', 'w') as f:
         f.write(SETUP_PY(package))
 
-    with open(os.path.join(package, '.gitignore'), 'w') as f:
+    with open('.gitignore', 'w') as f:
         f.write(GITIGNORE())
 
 
 @click.command()
-@click.argument("package")
+@click.argument("package", required=False)
 def main(package):
     """ Create a new python package template """
     dir_path = os.path.dirname(sys.modules[__name__].__file__)
