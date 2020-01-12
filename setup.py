5aedc9e6dd8315252c6359d5b7fec6e64ef3808e
Boccochan <boccochan@gmail.com>
2020-01-12 21:18:50 +0900

    Fix: entry_point path

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 import io
+import os
 import re
+import glob
 
 from setuptools import find_packages
 from setuptools import setup
 
+version_file_path = os.path.join('src', 'skepy', '__init__.py')
 with io.open("src/skepy/__init__.py", "rt", encoding="utf8") as f:
     version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
+
+template_files = []
+for file_name in glob.glob(os.path.join('src', 'skepy', 'template', '**'), recursive=True):
+    template_files.append(file_name.split(os.path.sep, 2)[-1])
 
 setup(
     name="skepy",
@@ -15,7 +22,6 @@ setup(
     maintainer="Yasuhiro Ito",
     maintainer_email="boccochan@gmail.com",
     description="Calculate time of python code.",
-    packages=find_packages("src"),
     install_requires=[
         "click",
     ],
@@ -24,7 +30,9 @@ setup(
             "pytest",
         ],
     },
-    package_dir={"": "src"},
-    python_requires="~=3.7",
-    entry_points={"console_scripts": ["skepy = skepy.cli:main"]},
+    packages=['skepy'],
+    package_dir={"skepy": "src/skepy"},
+    package_data={'skepy': template_files},
+    python_requires="~=3.8",
+    entry_points={"console_scripts": ["skepy = src.skepy.cli:main"]},
 )
